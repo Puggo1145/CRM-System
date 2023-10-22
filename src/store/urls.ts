6e0f8d1369fd7311bd572a62e68635a7e00208d9
Andrew Zhao <puggoo1145@gmail.com>
2023-10-22 23:32:28 +0800

    backendurl

--- src/store/urls.ts
+++ src/store/urls.ts
@@ -5,7 +5,7 @@ type urlsType = {
 }
 
 const useUrl = create<urlsType>((set) => ({
-    backendUrl: 'http://localhost:3000'
+    backendUrl: 'https://yuedajiaoyu.top:3000'
 }));
 
 export default useUrl;
